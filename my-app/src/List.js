27d133efd1275f507ae1ad93d7a56a9ca5af2f0f
Noah <Snoklkl@users.noreply.github.com>
2020-09-02 15:05:42 -0500

    Cleaneed out unneeded List.css, renamed files appropriate to the project

---
+++ my-app/src/List.js
@@ -0,0 +1,15 @@
+import React from 'react';
+
+import './List.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h2> To Do </h2>
+      </header>
+    </div>
+  );
+}
+
+export default App;
