b682dd3c0744d06a14010fe4edfd586d42a2354b
Noah <Snoklkl@users.noreply.github.com>
2020-09-02 19:40:31 -0500

    Revert "added bootstrap npm"
    This reverts commit e1e49370fd7da1ed58efc0bfdddca6c636fe7735.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -2,13 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 import './index.css';
-import App from './App.jsx';
-import Navigation from './components/Navigation.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import List from './List';
+
 
 ReactDOM.render(
   <React.StrictMode>
-   <App />
+   <List />
   </React.StrictMode>,
   document.getElementById('root')
 );
