138a48db877fc1c3449472a8a8614e194be65b56
Noah <Snoklkl@users.noreply.github.com>
2020-09-02 19:39:40 -0500

    Revert "hopefullyl revert"
    This reverts commit c8b7566445825cc3edd8736fa0cc425249c4dde5.

--- my-app/src/index.js
+++ my-app/src/index.js
@@ -4,7 +4,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App.jsx';
 import Navigation from './components/Navigation.jsx'
-//import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 ReactDOM.render(
   <React.StrictMode>
